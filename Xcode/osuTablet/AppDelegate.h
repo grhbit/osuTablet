1baf63b940a6fb8c14b53aabac6a369f67d62215
seonggwang.gwon <g.passcode@gmail.com>
2013-01-13 01:12:23 +0900

    Xcode 프로젝트 생성

---
+++ Xcode/osuTablet/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  osuTablet
+//
+//  Created by seonggwang on 13. 1. 13..
+//  Copyright (c) 2013년 senggwang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
